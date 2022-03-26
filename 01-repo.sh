d52bb968d23c8165ccb869800006c7323f65b0a1
TR Platt <trplatt@amazon.com>
2022-03-26 01:12:12 +0000

    made repo setup more robust by falling back to zip of source if github not avaialble

--- 01-repo.sh
+++ 01-repo.sh
@@ -18,10 +18,20 @@ PREFIX=a-new-startup-eks
 # Make sure we don't have this folder local
 rm -rf a-new-startup-github 
 
+echo "Attemping to retrieve latest a-new-startup app source code from git@github.com:tplatt37/a-new-startup.git using ssh..."
 git clone git@github.com:tplatt37/a-new-startup.git a-new-startup-github
-
-# NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
-cd a-new-startup-github && zip -r --exclude=*.git/* ../a-new-startup.zip ./* .[^.]* && cd ..
+if [ $? -eq 128 ]; then
+        echo "But... that failed, so we'll use a possibly out of date zip instead."
+        cp a-new-startup-fallback.zip a-new-startup.zip
+else
+        # If it was successful, zip up what was cloned
+        # NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
+        echo "Success! Let's zip it up!"
+        cd a-new-startup-github && zip -r --exclude=*.git/* ../a-new-startup.zip ./* .[^.]* && cd ..
+        # Save this for next time, in case we can't get the code live.
+        # (This makes the maintainer's life easier)
+        cp a-new-startup.zip a-new-startup-fallback.zip 
+fi
 
 aws s3 cp a-new-startup.zip s3://$BUCKET
 
@@ -30,10 +40,21 @@ aws s3 cp a-new-startup.zip s3://$BUCKET
 # Make sure we don't have this folder local
 rm -rf a-new-startup-eks-helm-github 
 
-git clone git@github.com:tplatt37/a-new-startup-eks-helm.git a-new-startup-eks-helm-github
 
-# NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
-cd a-new-startup-eks-helm-github && zip -r --exclude=*.git/* ../a-new-startup-helm.zip ./* .[^.]* && cd ..
+echo "Attemping to retrieve latest a-new-startup helm chart source code from git@github.com:tplatt37/a-new-startup-eks-helm.git using ssh..."
+git clone git@github.com:tplatt37/a-new-startup-eks-helm.git a-new-startup-eks-helm-github
+if [ $? -eq 128 ]; then
+        echo "But... that failed, so we'll use a possibly out of date zip instead."
+        cp a-new-startup-helm-fallback.zip a-new-startup-helm.zip
+else
+        # If it was successful, zip up what was cloned
+        # NOTE: When we zip, we ignore .git folder, but include other hidden files and folders! 
+        echo "Success! Let's zip it up!"
+        cd a-new-startup-eks-helm-github && zip -r --exclude=*.git/* ../a-new-startup-helm.zip ./* .[^.]* && cd ..
+        # Save this for next time, in case we can't get the code live.
+        # (This makes the maintainer's life easier)
+        cp a-new-startup-helm.zip a-new-startup-helm-fallback.zip 
+fi
 
 aws s3 cp a-new-startup-helm.zip s3://$BUCKET
 
